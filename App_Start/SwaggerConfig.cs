b71d99b1da68be4d0e4f148e3b376107fc4626af
900119 <anggun.perpatih@bni.co.id>
2023-12-17 11:27:06 +0700

    First Commit Back End Authentication and Registration using net6

---
+++ App_Start/SwaggerConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+using System.Web.Http;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle;
+using Swashbuckle.Application;
+namespace WebApi.App_Start
+{
+    public class SwaggerConfig
+    {
+        public static void Register()
+        {
+            var thisAssembly = typeof(SwaggerConfig).Assembly;
+            
+            GlobalConfiguration.Configuration
+              .EnableSwagger(c => c.SingleApiVersion("v1", "ErrorHandlingWebAPI"))
+              .EnableSwaggerUi();
+        }
+    }
+}
